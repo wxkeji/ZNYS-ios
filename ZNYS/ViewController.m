7f13025a405e59feeb40272a41d5e621a0b015f6
TianQiLi <871651575@qq.com>
2015-08-07 10:54:41 +0800

    Project set up

---
+++ ZNYS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZNYS
+//
+//  Created by mac on 15/8/7.
+//  Copyright (c) 2015年 Woodseen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
