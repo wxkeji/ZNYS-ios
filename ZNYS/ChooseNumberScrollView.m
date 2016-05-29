aeedb08fa9bbdce15ae7d40b1ab35918f6247813
黄丕臻 <huangpizhen@maka.im>
2016-05-29 12:33:41 +0800

    no message

--- ZNYS/ChooseNumberScrollView.m
+++ ZNYS/ChooseNumberScrollView.m
@@ -21,15 +21,15 @@
         self.backgroundColor = [UIColor clearColor];
         self.pagingEnabled = YES;
         self.scrollEnabled = YES;
-        self.contentSize = CGSizeMake(CustomWidth(106), CustomHeight(30)*(model.range+2));
+        self.contentSize = CGSizeMake(CustomWidth(106), (model.range+3)*CustomHeight(30));
         
-        for (NSInteger i = model.coins-1, j = 0; j <= model.range+2 ; i++,j++) {
+        for (NSInteger i = model.coins-1, j = 0; j < model.range+2 ; i++,j++) {
             UIView * v = [[UIView alloc] initWithFrame:CGRectMake(0, j*CustomHeight(30), CustomWidth(106), CustomHeight(30))];
             v.backgroundColor = [UIColor clearColor];
             
             UILabel * label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, CustomWidth(106), CustomHeight(30))];
             label.backgroundColor = [UIColor clearColor];
-            if (i == (model.coins-1) || i == model.range+2+model.coins) {
+            if (i == (model.coins-1) || i == model.range+1+model.coins) {
                 label.text = @" ";
             }else{
              label.text = [NSString stringWithFormat:@"%ld",(long)i];
