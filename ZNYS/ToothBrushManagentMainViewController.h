9a0514aabc51a9a829fc0f654ec1fdbb69f78175
EricCheungSCUT <543198902@qq.com>
2016-06-21 11:30:45 +0800

    牙刷管理模块 startup

---
+++ ZNYS/ToothBrushManagentMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  ToothBrushManagentMainViewController.h
+//  ZNYS
+//
+//  Created by 张恒铭 on 6/12/16.
+//  Copyright © 2016 Woodseen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToothBrushManagentMainViewController : UIViewController
+
+@end
